89fd04aad9c50e226476cf28ec6cf8a44cdf6cde
Ramazan-lab <83654016+Ramazan-lab@users.noreply.github.com>
2021-05-04 22:55:03 +0300

    Add files via upload

---
+++ matrisçarpma.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+
+
+
+	int main(){	
+	int a[5][5]={for(int i=0;i<25;i++{cout << rand() << endl})} , b[3][3]={-1,2,3,-2,1,4,5,2,-2};
+	int i,j,k,l;
+	int sonuc;
+	
+	for( i=0;i<3;i++){
+	
+		for( j=0;j<3;j++){
+			
+	printf("a[%d][%d]=",i,j);					
+	}
+	printf("\n");		
+	}	
+	
+	
+	
+	for( k=0;k<5;k++){
+	
+		for( l=0;l<5;l++){
+			
+	printf("b[%d][%d]=",k,l);		
+	}		
+	printf("\n");		
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
